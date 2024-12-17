b5993e8a62bcd504d9b2e9c60f1c4b9d55601264
swapno963 <swapno963@gmail.com>
2024-12-17 15:41:26 +0600

    nacesssary data for frontend developer done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
     path('create-table/', create_table_view, name='create_table'),
     path('<str:table_name>/add-row/', add_row_view, name='add_row'),
     path('<str:table_name>/list-rows/', list_rows_view, name='list_rows'),
+
+
+    # for frontend developer
+    path('count_info/', FileInfoCountAPIView.as_view(), name='file_count_info'), # count
+    path('bar_chart/', FileInfoBarChartAPIView.as_view(), name='bar_chart'), # bra-chart 
+    path('line_chart/', TagInfoLineChartAPIView.as_view(), name='line_chart'), # line-chart
+    path('tag_relation/', TagRelationAPIView.as_view(), name='tag_relation'), # tag-relation
+    path('file_percentage/', FileInfoPercentageAPIView.as_view(), name='file_dsshbord'), # file_percentage
+    path('overall_analytics/',OverallAnalyticsAPIView.as_view(), name='overall_analytics'), # overall_analytics
+
    
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,9 @@ from django.http import JsonResponse
 from .dynamic_model_creator import create_dynamic_table
 from django.contrib.auth.decorators import login_required
 from django.db import models
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 @login_required
 def create_table_view(request):
@@ -49,3 +52,90 @@ def list_rows_view(request, table_name):
     # Filter rows based on the current user
     rows = DynamicModel.objects.filter(created_by=request.user)
     return render(request, 'list_rows.html', {'rows': rows, 'table_name': table_name})
+
+
+
+
+# file info count
+class FileInfoCountAPIView(APIView):
+    def get(self, request, format=None):
+        data = {
+            "documents": 30,
+            "tags": 21,
+            "workflow": 25,
+            "tag_relations": 28,
+        }
+        return Response(data)
+ 
+
+class FileInfoBarChartAPIView(APIView):
+    def get(self, request, format=None):
+        data = {
+        "file_count": [
+                    {"file_type": "pdf", "count": 14},
+                    {"file_type": "docx", "count": 8},
+                    {"file_type": "xlsx", "count": 6},
+                    {"file_type": "png", "count": 13},
+                    {"file_type": "jpg", "count": 11},
+                    {"file_type": "zip", "count": 7},
+                    {"file_type": "txt", "count": 10}
+      ]
+        }
+        return Response(data)
+    
+
+class TagInfoLineChartAPIView(APIView):
+    def get(self, request, format=None):
+        data = {
+            "tag_counts": [
+                        {"tag": "important", "count": 5},
+                        {"tag": "confidential", "count": 3},
+                        {"tag": "archive", "count": 4},
+                        {"tag": "review", "count": 2},
+                        {"tag": "shared", "count": 3},
+                        {"tag": "urgent", "count": 6},
+                        {"tag": "public", "count": 2}
+  ]
+        }
+        return Response(data)
+ 
+
+class TagRelationAPIView(APIView):
+    def get(self, request, format=None):
+        data = {
+            "equipment": 192,
+            "instruments": 5000,
+            "pipelines": 32,
+        }
+        return Response(data)
+ 
+
+class FileInfoPercentageAPIView(APIView):
+    def get(self, request, format=None):
+        data = {
+            "file": [
+    {"file_type": "pdf", "percentage": 11.54},
+    {"file_type": "docx", "percentage": 16.67},
+    {"file_type": "xlsx", "percentage": 12.82},
+    {"file_type": "png",  "percentage": 10.26},
+    {"file_type": "jpg",  "percentage": 19.23},
+    {"file_type": "zip",  "percentage": 7.69},
+    {"file_type": "txt",  "percentage": 15.38}
+]
+
+        }
+        return Response(data)
+    
+
+class OverallAnalyticsAPIView(APIView):
+    def get(self, request, format=None):
+        data = {
+            "file_list": 100,
+            "documents": 40,
+            "tag_relation": 80,
+            "worflow": 5,
+            "document_type": 2,
+            "tag_type": 8,
+        }
+        return Response(data)
+ 
